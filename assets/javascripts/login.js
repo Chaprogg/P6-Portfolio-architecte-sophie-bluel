07cb6b655324e1d1a09036b397b7d4d45163c99b
Chaprogg <chbbbmo@orange.fr>
2024-09-25 15:57:14 +0200

    Add files via upload

--- assets/javascripts/login.js
+++ assets/javascripts/login.js
@@ -1,8 +1,8 @@
 // Access DOM LOGIN
-const form = document.querySelector("form")
-const errorMessage = document.querySelector(".errorLogin")
-const email = document.getElementById("email")
-const password = document.getElementById("password")
+const form = document.querySelector("form");
+const errorMessage = document.querySelector(".errorLogin");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 
 //AddEventListener for Email and Password Input Form
 form.addEventListener("submit", (event) => {
@@ -18,40 +18,43 @@ form.addEventListener("submit", (event) => {
     const user = JSON.stringify(login);
 
 
-    //Fetch Request Server (same-origin= validate login)
-    fetch("http://localhost:5678/api/users/login", {
-        method: "post",
-        mode: "cors",
-        credentials: "same-origin",
-        headers: { "Content-Type": "application/json" },
-        body: user,
-    })
-    //Response recover Database
-    .then((ServerResponse) => {
-        if (!ServerResponse.ok) {
-        const errorLogin = document.querySelector("p");
-        errorLogin.textContent = "L'Email ou le Mot de passe sont incorrects.";
+//Fetch Request Server (same-origin= validate login)
+fetch("http://localhost:5678/api/users/login", {
+    method: "post",
+    mode: "cors",
+    credentials: "same-origin",
+    headers: { "Content-Type": "application/json" },
+    body: user,
+})
 
-        throw new Error();
-        }
-        // Json Parse
-        return ServerResponse.json(); 
+//Response recover Database
+.then((ServerResponse) => {
+    if (!ServerResponse.ok) {
+    const errorLogin = document.querySelector("p");
+    errorLogin.textContent = "L'Email ou le Mot de passe sont incorrects.";
+
+      throw new Error();
+    }
+    // Json Parse
+    return ServerResponse.json(); 
+})
+
+// Recover Data Id and Token of user
+.then((data) => {
+    const userId = data.userId;
+    const userToken = data.token;
+    window.sessionStorage.setItem("userId", userId);
+    window.sessionStorage.setItem("token", userToken);
+    window.location.href = "index.html";
     })
-    // Recover Data Id and Token of user
-    .then((data) => {
-        const userId = data.userId;
-        const userToken = data.token;
-        window.sessionStorage.setItem("userId", userId);
-        window.sessionStorage.setItem("token", userToken);
-        window.location.href = "index.html";
-        })
-        .catch(() => {
-        console.error("Une erreur est survenue");
-        
+
+    .catch(() => {
+    console.error("L'email ou le mot de passe sont manquants ou invalides");
     });
 })
+
 // Display Modal at click on Modifier Button
-login.addEventListener("click", () => {
-    containerModal.style.display = "flex"
-})
-console.log(sessionStorage)
+//login.addEventListener("click", () => {
+  //  containerModal.style.display = "flex"
+//})
+//console.log(sessionStorage)
